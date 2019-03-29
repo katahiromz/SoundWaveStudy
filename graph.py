e92fb74e7ac8cb9ed47cfb84b530c74ec31318c2
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-03-29 17:12:22 +0900

    fix graph.py

--- graph.py
+++ graph.py
@@ -3,6 +3,6 @@ import matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
-data = pd.read_csv(sys.argv[1], header=None, sep='\t')
+data = pd.read_csv(sys.argv[1], header=None, sep=' ')
 data.plot()
 plt.show()
